c5a8bdc6af380fe3f135e2f1a4e6e044a582f9e2
Carson Gross <carsongro@gmail.com>
2023-12-21 15:58:54 -0700

    Initial Commit

---
+++ MusicVisualizer/MusicVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicVisualizerApp.swift
+//  MusicVisualizer
+//
+//  Created by Carson Gross on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
